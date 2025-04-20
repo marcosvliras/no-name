c23a04e3187ceb58c38a39bdf2e1911afccb5327
Marcos Vinicius <84594190+marcosvliras@users.noreply.github.com>
2025-04-20 09:25:09 -0300

    feat: update server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/marcosvliras/no-name/internal/controllers"
+	"github.com/marcosvliras/no-name/internal/service"
 )
 
 func main() {
 	server := gin.Default()
 
-	server.GET("/health", controllers.HealthCheck)
+	svc := service.NewAlphavantageSVC()
+	stockCtrl := controllers.NewStocksCtrl(svc)
 
-	server.Run(":8000")
+	server.GET("/health", controllers.HealthCheck)
+	server.GET("/stocks", stockCtrl.Handle)
+
+	if err := server.Run(":8000"); err != nil {
+		log.Fatalf("failed to start server: %v", err)
+	}
 }
