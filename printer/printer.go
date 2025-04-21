60d6bfcaee13b711335a1f26f10448616fbad0cb
Marcos Vinicius <84594190+marcosvliras@users.noreply.github.com>
2025-04-21 08:02:28 -0300

    feat: update print output for cli

--- printer/printer.go
+++ printer/printer.go
@@ -3,6 +3,10 @@ package printer
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"text/tabwriter"
+
+	"github.com/marcosvliras/sophie/stock"
 )
 
 func StdoutPrint(v interface{}) {
@@ -12,4 +16,24 @@ func StdoutPrint(v interface{}) {
 		return
 	}
 	fmt.Println(string(data))
+}
+
+func PrintAggStockDataTable(data []stock.AggStockData) {
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
+
+	fmt.Fprintln(w, "STOCK\tACTUAL PRICE\tMAX PRICE")
+
+	for _, item := range data {
+		max := "N/A"
+		actual := "N/A"
+		if item.MaxStockPrice != nil {
+			max = fmt.Sprintf("%.2f", *item.MaxStockPrice)
+		}
+		if item.ActualPrice != nil {
+			actual = fmt.Sprintf("%.2f", *item.ActualPrice)
+		}
+		fmt.Fprintf(w, "%s\t%s\t%s\n", item.Stock, actual, max)
+	}
+
+	w.Flush()
 }
